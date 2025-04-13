92ab1b38d72581a91d44a0fa1ec69b5d6b2ec507
Umutcan Özer <102672243+umutcanozer@users.noreply.github.com>
2025-04-13 19:31:35 +0300

    Delete Engine directory

--- Engine/IMessageHandler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Windows.h>
-
-class IMessageHandler {
-public:
-    virtual void OnMessage(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) = 0;
-    virtual ~IMessageHandler() = default;
-};
--- Engine/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <Windows.h>
-#include "Engine.h"
-
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	Engine engine;
-	engine.Run();
-}
